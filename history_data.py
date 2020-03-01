e58ea114285ad9d92213a2fd03923e14e03fddd3
aboutmonero <aboutmonero@protonmail.com>
2020-03-01 10:19:35 -0600

    added atomic units and removed some learn topics

--- history_data.py
+++ history_data.py
@@ -10,7 +10,8 @@ from datetime import datetime
 def get_csv(direc):
     with open("static/data/"+direc+".csv", 'r') as f:
         reader = csv.reader(f)
-        rows=list(reader)
+        rows = list(reader)
+    rows = [[float(y) for y in x] for x in rows]
     return rows
         
 def cache(data,direc):
@@ -30,57 +31,68 @@ def get_price():
     prices = [[float(x[0]),float(x[4])] for x in list(data['result']['14400'])]
     cache(prices,"price")
         
-def get_current_block():
-    data = get_json("https://www.xmrchain.net/api/networkinfo")
-    return int(data['data']['height'])
-        
 def get_blocks():
-    
-    def get_block_reward():
-        blocks = get_csv("blocks")
-        data = [[x[0],x[3]] for x in blocks]
-        last = len(get_csv("reward"))
-        data = data[last-1369999:]
-        cache(data,"reward")
-        
-    def get_supply():
-        reward = get_csv("reward")
-        supply = [[0.0,0.0]]
-        last = len(get_csv("supply"))
-        reward = reward[last:]
-        for x in reward:
-            supply.append([x[0],float(x[1]) + supply[-1][1]])
-        cache(data,"reward")
-
-    def get_hashrate():
-        blocks = get_csv("blocks")
-        data = [[x[1],float(x[2])/120] for x in blocks]
-        cache(data,"hashrate")
-        
-    def get_transactions():
-        blocks = get_csv("blocks")
-        data = [[x[1],float(x[4])] for x in blocks]
-        cache(data,"transactions")
-        
     daemon = Daemon(JSONRPCDaemon(port=18081))
-    last = 2000000
+    last = 1500000
     new = []
-    current = 2030000
+    current = 2045023
     while current > last:
         data = daemon.block(height=last)
         new.append([datetime.fromisoformat(str(data.timestamp)).timestamp(),data.height,data.difficulty,float(data.reward),len(data.transactions)])
         last +=1
-        if(last%1000==0):
+        if(last%10000==0):
+            cache(new,"blocks")
+            new = []
             print(last)
-    cache(new,"blocks")
+
     return True
 
-def convert_to_month(direc):
-    data = get_csv(direc)
+def get_block_reward():
+    blocks = get_csv("blocks")
+    data = [[x[0],x[3]] for x in blocks]
+    return data
+
+def get_block_time():
+    blocks = get_csv("blocks")
+    data = [[blocks[0][0],120]]
+    for x in blocks[1:]:
+        data.append([x[0],(x[0]-data[-1][0])+1])
+    data = [[data[i][0],sum([data[i-j][1] for j in range(60)])/60] for i in range(60,len(data))]
+    data = data[:60]+data
+    return data 
+
+def get_difficulty():
+    blocks = get_csv("blocks")
+    data = []
+    for x in blocks:
+        data.append([x[0],x[2]])
+    return data 
+    
+def get_supply():
+    reward = get_block_reward()
+    supply = [[reward[0][0],0.0]]
+    for x in reward[1:]:
+        supply.append([x[0],x[1] + supply[-1][1]])
+    return supply
+
+def get_hashrate():
+    block_time = get_block_time()
+    difficulty = get_difficulty()
+    data = [[difficulty[0][0],difficulty[0][1]/(block_time[0][0])]]
+    for i in range(len(difficulty[1:])):
+        data.append([difficulty[i][0],difficulty[i][1]/(block_time[i][1])])
+    return data 
+    
+def get_transactions():
+    blocks = get_csv("blocks")
+    data = [[x[0],x[4]] for x in blocks]
+    return data
+    
+def convert_to_month(data):
     start = time.time() - 30*24*60*60
 
     # find earliest block
-    i = len(data)
+    i = len(data) - 1
     while float(data[i][0]) > start:
         i -= 1
         
@@ -91,9 +103,54 @@ def convert_to_month(direc):
     l = int(len(data)/360)
     data = [data[i] for i in range(len(data)) if i % l == 0]
     
-    cache(month_data,direc+"_1m")
-    return month_data    
+    return data    
         
+
+def convert_to_year(data):
+    last_month = convert_to_month(data)
+    
+    start = time.time() - 360*24*60*60
+
+    # find earliest block
+    i = len(data) - 1
+    while float(data[i][0]) > start:
+        i -= 1
+        
+    # cut off earlier blocks
+    data = data[i:]
+    
+    #find last block
+    finish = time.time() - 30*24*60*60
+    i = len(data) - 1
+    while float(data[i][0]) > finish:
+        i -= 1
+    data = data[:i]
+    
+    # want 360 data points
+    l = int(len(data)/360)
+    data = [data[i] for i in range(len(data)) if i % l == 0]
+    
+    data.append(last_month)
+    return data    
+        
+
+def convert_to_all(data):
+    last_year = convert_to_year(data)
+
+    #find last block
+    finish = time.time() - 360*24*60*60
+    i = len(data) - 1
+    while float(data[i][0]) > finish:
+        i -= 1
+    data = data[:i]
+    
+    # want 360 data points
+    l = int(len(data)/360)
+    data = [data[i] for i in range(len(data)) if i % l == 0]
+    
+    data.append(last_year)
+    
+    return data
         
 '''      
 get_blocks()
